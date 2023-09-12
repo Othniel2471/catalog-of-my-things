cd608af2d925faa3e0b9e50ab16c4d80835b532e
Muhammad Amran Khan (Mike) <Tipu870@gmail.com>
2023-09-12 20:13:33 +0500

    Update game.rb

--- game.rb
+++ game.rb
@@ -2,15 +2,16 @@ require 'date'
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :multiplayer, :last_played_at
+  attr_accessor :publish_date, :multiplayer, :last_played_at, :archived
 
   def initialize(publish_date, multiplayer, last_played_at)
     super(publish_date) # Call the parent class's constructor
     @multiplayer = multiplayer
     @last_played_at = last_played_at
+    @archived = false # Initialize archived to false by default
   end
 
   def can_be_archived?
-    super && (Date.today - @last_played_at).to_i > 730
+    (Date.today - @last_played_at).to_i > 730
   end
 end
