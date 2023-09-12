0ff6c29a61e2f4d13f2359276e8a45108f6abd56
othniel2471 <othnielomaidu@gmail.com>
2023-09-12 10:05:29 +0100

    added music_album class and genre class

---
+++ genre.rb
@@ -0,0 +1,13 @@
+class Genre
+  attr_accessor :name, :items
+
+  def initialize(name)
+    @name = name
+    @items = []
+  end
+
+  def add_item(item)
+    item.genre = self
+    @items << item
+  end
+end
