6d4f4a0a1694b349b7753ff3ee78965ed89f7a88
Aamir <aamirasaram2107@gmail.com>
2023-09-14 15:40:30 +0200

    Refactor game logic

--- game.rb
+++ game.rb
@@ -2,16 +2,18 @@ require 'date'
 require_relative 'item'
 
 class Game < Item
-  attr_accessor :publish_date, :multiplayer, :last_played_at, :archived
+  attr_accessor :publish_date, :multiplayer, :last_played_at, :author
 
-  def initialize(publish_date, multiplayer, last_played_at)
-    super(publish_date) # Call the parent class's constructor
+  def initialize(publish_date, multiplayer, last_played_at, author = nil)
+    publish = publish_date.split('-')
+    super(Date.new(publish[0].to_i, publish[1].to_i, publish[2].to_i)) # Call the parent class's constructor
     @multiplayer = multiplayer
-    @last_played_at = last_played_at
-    @archived = false # Initialize archived to false by default
+    date = last_played_at.split('-')
+    @last_played_at = Date.new(date[0].to_i, date[1].to_i, date[2].to_i)
+    self.author = author if author
   end
 
   def can_be_archived?
-    (Date.today - @last_played_at).to_i > 730
+    !@last_played_at.nil? && Date.today.year - @last_played_at.year > 2
   end
 end
